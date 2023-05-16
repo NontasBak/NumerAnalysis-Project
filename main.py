dcc045e4d659b771f50695a62fdc38dad87dff01
NontasBak <nontasbak@gmail.com>
2023-05-16 18:00:39 +0300

    Fix QRmine function

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plot
+
+def LUmine(A):
+    n = A.shape[0]
+    L = np.matrix( np.identity(n) )
+    U = A
+    for j in range(0,n-1):
+        for i in range(j+1,n):
+            mult = A[i,j] / A[j,j]
+            A[i, j+1:n] = A[i, j+1:n] - mult * A[j, j+1:n]
+            U[i, j+1:n] = A[i, j+1:n]
+            L[i,j] = mult
+            U[i,j] = 0
+    return L, U
+
+def QRmine(A):
+    n = A.shape[0]
+    Q = np.matrix( np.zeros( (n,n) ) )
+    for j in range(0, n):
+        q = A[:,j]
+        for i in range(0, j):
+            length_of_leg = np.sum( A[:,j].T * Q[:,i])
+            q = q - length_of_leg * Q[:,i]
+        Q[:,j] = q / np.linalg.norm(q)
+    R = Q.T * A
+    return Q, R
+
+A = np.matrix([[1,1,0] , [1,0,1] , [0,1,1]])
+r = np.linalg.qr(A)
+print(r[0])
+print(r[1])
+
+test = QRmine(A)
+print(test[0])
+print(test[1])
