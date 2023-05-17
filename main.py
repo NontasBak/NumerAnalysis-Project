116750df6374e084837127dc0f5542736195a287
NontasBak <nontasbak@gmail.com>
2023-05-17 11:20:27 +0300

    Implement Hilbert and SolveWithQR/LU functions

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def QRmine(A):
     R = Q.T * A
     return Q, R
 
+'''
 A = np.matrix([[1,1,0] , [1,0,1] , [0,1,1]])
 r = np.linalg.qr(A)
 print(r[0])
@@ -35,3 +36,46 @@ print(r[1])
 test = QRmine(A)
 print(test[0])
 print(test[1])
+'''
+
+def Hilbert(n):
+    H = np.matrix( np.zeros( (n,n) ) )
+    for i in range(0,n):
+        for j in range(0,n):
+            H[i,j] = 1 / (i + j + 1)
+    return H
+
+'''
+Hil = Hilbert(5)
+print(Hil)
+'''
+
+def solveWithQR_Axb(A, b):
+    QR = QRmine(A)
+    x = np.linalg.inv(QR[1]) * QR[0].T * b
+    return x
+
+def solvewithLU_Axb(A, b):
+    LU = LUmine(A)
+    x = np.linalg.inv(LU[1]) * np.linalg.inv(LU[0]) * b
+    return x
+
+'''
+n = 4
+Hil = Hilbert(n)
+b = np.ones((n, 1))
+solution1 = solveWithQR_Axb(Hil, b)
+
+print(solution1)
+print(Hil * solution1)
+
+
+solution2 = solvewithLU_Axb(Hil, b)
+Hil = Hilbert(n) #H A=LU xalaei ton pinaka A
+
+print(solution2)
+print(Hil * solution2)
+'''
+
+
+
